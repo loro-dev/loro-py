2f434ee14cb9b97bc6de6cb6e6b5cc9a80a8f343
Michael Hahn <mwhahn@gmail.com>
2025-01-15 10:04:13 -0800

    Add failing test

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,4 +1,18 @@
-from loro import LoroDoc, StyleConfigMap, TextDelta
+from loro import ExportMode, LoroDoc, LoroMap, LoroText, StyleConfigMap, TextDelta, ValueOrContainer
+
+class Wrapper:
+
+    def __init__(self, doc: LoroDoc):
+        self.doc = doc
+
+    @classmethod
+    def from_snapshot(cls, snapshot: bytes) -> 'Wrapper':
+        doc = LoroDoc()
+        doc.import_(snapshot)
+        return cls(doc)
+
+    def export(self) -> bytes:
+        return self.doc.export(ExportMode.Snapshot())
 
 
 def test_text_get_value():
@@ -36,3 +50,11 @@ def test_text_to_delta():
             assert isinstance(delta, TextDelta.Insert)
             assert delta.insert == " world!"
             assert delta.attributes == None
+
+def test_wrapper():
+    wrapper = Wrapper(LoroDoc())
+    text = LoroText()
+    text.insert(0, 'Hello world!')
+    wrapper.doc.get_map('fields').insert_container('text', text)
+
+    Wrapper.from_snapshot(wrapper.export()).doc.get_map('fields').get_deep_value()
